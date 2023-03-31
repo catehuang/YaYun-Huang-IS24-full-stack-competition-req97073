59cc4c448db951553db00ae4fcce79cc7581a4c5
catehuang <s0350228@gmail.com>
2023-03-30 18:27:29 -0600

    fix put api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing.js';
+import Home from './pages/Home.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
