406e5f570bec35b689a5852527530198043b6763
catehuang <s0350228@gmail.com>
2023-04-15 22:01:46 -0600

    sort data by productId

--- src/api_calls/product.js
+++ src/api_calls/product.js
@@ -1,53 +1,58 @@
 import { axios } from "./axios.js";
 
-export const GetAllProducts = async() => {
-    try {
-        const response = await axios.get('/product');
-        return response.data;
-      } catch (error) {
-        console.log(error.message);
-      }
-}
-
-export const GetProduct = async (productId) => {
+export const GetAllProducts = async () => {
   try {
-    const response = await axios.get(`/product/${productId}`);
-    return response.data
+    const response = await axios.get("/product");
+    // Sort products by id
+    const products = response.data;
+    const sorted = products.slice(0).sort(function (a, b) {
+      return a.productId < b.productId ? -1 : a.productId > b.productId ? 1 : 0;
+    });
+    return sorted;
   } catch (error) {
     console.log(error.message);
   }
 };
 
-export const AddNewProduct = async(product) => {
-  console.log(product)
+export const GetProduct = async (productId) => {
   try {
-      const response = await axios.post('/product', {
-        ...product
-      });
-      return response.data;
-    } catch (error) {
-      console.log(error.message);
-    }
-}
+    const response = await axios.get(`/product/${productId}`);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const AddNewProduct = async (product) => {
+  console.log(product);
+  try {
+    const response = await axios.post("/product", {
+      ...product,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const UpdateProduct = async (product) => {
-  const {productId, ...others} = product
+  const { productId, ...others } = product;
 
   try {
     const response = await axios.put(`/product/${productId}`, {
       ...others,
     });
-    return response.data
+    return response.data;
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 
-export const DeleteProduct= async (productId) => {
+export const DeleteProduct = async (productId) => {
   try {
     const response = await axios.delete(`/product/${productId}`);
-    return response.data
+    return response.data;
   } catch (error) {
     console.log(error.message);
   }
-}
+};
